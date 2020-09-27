46ec9a8c4c1e288a01adc9568c33dee67a107eb1
pepo4343 <ptangwongkit@gmail.com>
2020-09-27 18:46:54 +0700

    delete nodemodule

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const { login } = require("./controllers/user");
 const User = require("./models/user");
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Methods",
         "OPTIONS, GET, POST, PUT, PATCH, DELETE"
